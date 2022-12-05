ce8f7068f86aaeb63521dcac67cec270909a43e7
Yotam-D <d.yotam@gmail.com>
2022-12-05 15:49:01 +0200

    set enviroment for sending scheduled emails

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv')
 const axios = require('axios')
 const cors = require('cors')
 const cheerio = require('cheerio')
+const cron = require('node-cron')
+const nodemailer = require('nodemailer');
 const app = express()
 dotenv.config()
 // const knex = require('knex')
@@ -14,9 +16,9 @@ app.use(cors())
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.json())
 
-app.listen (process.env.PORT, ()=>{
-    console.log(`listening on port ${process.env.PORT}`);
-})
+// app.listen (process.env.PORT, ()=>{
+//     console.log(`listening on port ${process.env.PORT}`);
+// })
 
 
 const getPostTitles = async () => {
@@ -40,11 +42,35 @@ const getPostTitles = async () => {
 		throw error;
 	}
 };
-
 getPostTitles()
     .then((postTitles) => console.log(postTitles));
 
 function reverseString(string) {
-    return string.split("").reverse().join("").replace(/(\r\n|\n|\r|\t)/gm, "");
+    return string.split("").reverse().join("").replace(/(\r\n|\n|\r|\t)/gm, "");  //reverses the string, remove string operations
 };
-    // console.log(reverseInPlace("abd fhe kdj"))
+
+let mailOptions = {
+	from: 'd.yotam@gmail.com',
+	to: 'd.yotam@gmail.com',
+	subject: 'Email from Node-App: A Test Message!',
+	text: 'Some content to send',
+	html: '<b>The html content</b>'
+  };
+  
+  // Mail transport configuration
+  let transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+	  user: 'd.yotam@gmail.com',
+	  pass: process.env.PASS,
+	},
+	tls: {
+	  rejectUnauthorized: false,
+	},
+  });
+  
+  // Delivering mail with sendMail method
+//   transporter.sendMail(mailOptions, (error, info) => {
+// 	if (error) console.log(error);
+// 	else console.log('Email sent: ' + info.response);
+//   });
